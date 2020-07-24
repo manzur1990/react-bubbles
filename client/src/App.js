62f215cdcdf6c4b4f62e5e81a83e88be4fc84b19
JM <manzur-1990@hotmail.com>
2020-07-24 10:50:41 -0500

    login page is working as intended

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
+import React from "react";
 import "./styles.scss";
+
+//Routing
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+
+// Components
+import BubblePage from "./components/BubblePage";
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute patch={"/bubble-page"} component={BubblePage} />
       </div>
     </Router>
   );
