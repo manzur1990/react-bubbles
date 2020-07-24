090f70e3a52aa65b5cbf06279d64629f5edfdbcc
JM <manzur-1990@hotmail.com>
2020-07-24 11:41:03 -0500

    saveEdit feature added

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 
 //Components
@@ -7,6 +8,7 @@ import ColorList from "./ColorList";
 import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
+  const { push } = useHistory()
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
@@ -19,12 +21,18 @@ const BubblePage = () => {
       .catch(err => console.log(err));
   }, []);
 
+  const logout = e => {
+    localStorage.removeItem('token');
+    push('/');
+  };
 
   return (
-    <>
+    <div>
+      <h1>Welcome to React Bubbles</h1>
+      <button onClick={logout}>Logout</button>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
-    </>
+    </div>
   );
 };
 
